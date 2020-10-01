3a864ead6dabad140a619ce1ab54c21c4d1cd2f1
great084 <great084@gmail.com>
2020-10-01 14:42:35 +0900

    編集画面の作成

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -24,9 +24,19 @@ class EstimatesController < ApplicationController
   end
 
   def edit
+    @estimate = Estimate.find(params[:id])
   end
 
   def update
+    @estimate = Estimate.find(params[:id])
+    if @estimate.update_attributes(estimate_params)
+      flash.now[:success] = "見積を更新しました"
+      redirect_to estimate_path(@estimate)
+    else
+      puts "#{@estimate.errors.full_messages}"
+      flash.now[:danger] = "見積更新できませんでした"
+      render :edit
+    end
   end
 
   private
--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -1,4 +1,7 @@
 class Estimate < ApplicationRecord
   belongs_to :user
   has_many :estimate_details
+
+  validates :subject, presence: true
+  validates :customer_name, presence: true
 end
