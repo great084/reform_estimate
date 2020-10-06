3b464cb21e57e857b44b6018f44b561c256916e7
ひろみち <36526480+great084@users.noreply.github.com>
2020-10-07 01:13:57 +0900

    一括登録フォームの変更（更新時の処理変更,Formモデルの廃止) (#15)
    * cocoonのインストール
    
    * jQueryの導入
    
    * 一括登録フォームの実装
    
    * 明細の一括更新（削除含む）を実装
    
    * 一括登録フォームの変更（更新時の処理変更,Formモデルの廃止)

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -5,6 +5,7 @@ class EstimatesController < ApplicationController
 
   def show
     @estimate = Estimate.find(params[:id])
+    @estimate_details = EstimateDetail.where(estimate_id: params[:id])
   end
 
   def new
@@ -13,12 +14,10 @@ class EstimatesController < ApplicationController
 
   def create
     @estimate = Estimate.new(estimate_params)
+    # debugger
     if @estimate.save
-      flash.now[:success] = "見積を作成しました"
-      redirect_to estimates_path
+      redirect_to estimates_path, notice: "見積 #{@estimate.subject}を登録しました"
     else
-      puts "#{@estimate.errors.full_messages}"
-      flash.now[:danger] = "見積作成に失敗しました"
       render :new
     end
   end
@@ -29,7 +28,8 @@ class EstimatesController < ApplicationController
 
   def update
     @estimate = Estimate.find(params[:id])
-    if @estimate.update_attributes(estimate_params)
+    # debugger
+    if @estimate.update!(estimate_params)
       flash.now[:success] = "見積を更新しました"
       redirect_to estimate_path(@estimate)
     else
@@ -42,7 +42,12 @@ class EstimatesController < ApplicationController
   private
 
   def estimate_params
-    params.require(:estimate).permit(:user_id, :subject, :customer_name)
+    params
+      .require(:estimate)
+      .permit(
+        Estimate::REGISTRABLE_ATTRIBUTES +
+        [estimate_details_attributes: EstimateDetail::REGISTRABLE_ATTRIBUTES]
+      )
   end
 
 end
--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -1,7 +1,18 @@
 class Estimate < ApplicationRecord
+  REGISTRABLE_ATTRIBUTES = %i(user_id subject customer_name)
   belongs_to :user
-  has_many :estimate_details
+  has_many :estimate_details, dependent: :destroy
+  accepts_nested_attributes_for :estimate_details, allow_destroy: true
+  before_validation :calculate_estimate_price
 
   validates :subject, presence: true
   validates :customer_name, presence: true
+
+  private
+
+  def calculate_estimate_price
+    estimate_details.each(&:calculate_estimate_detail_price)
+    self.subtotal = estimate_details.map { |ed| ed.price unless ed.marked_for_destruction?}.compact.sum
+  end
+
 end
