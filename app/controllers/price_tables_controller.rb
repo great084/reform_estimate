30241b4e7d2b0fb04bf3610e33b5bb8631629943
great084 <great084@gmail.com>
2020-10-17 01:48:13 +0900

    マスター登録画面の分離

--- app/controllers/price_tables_controller.rb
+++ app/controllers/price_tables_controller.rb
@@ -1,31 +1,58 @@
 class PriceTablesController < ApplicationController
+  before_action :set_price_table, only: %i[update destroy]
+
+  def index
+    @price_tables = current_user.price_tables
+    @price_table = PriceTable.new
+  end
+
   def create
     # binding.pry
     @price_table = PriceTable.new(price_table_params)
+    # binding.pry
     if @price_table.save
-      redirect_to masters_path
+      flash[:success] = '登録しました。'
+      redirect_to price_tables_path
     else
-      render 'masters/index'
+      render_index('登録に失敗しました。')
     end
   end
 
   def update
     # binding.pry
-    @price_table = PriceTable.find(params[:id])
     if @price_table.update(price_table_params)
-      redirect_to masters_path
+      flash[:success] = '更新しました。'
+      redirect_to index
     else
-      # ここのエラー時の処理は要改善。masters/indexでは@price_tables（複数）をレンダリングしているため
-      render 'masters/index'
+      render_index('更新に失敗しました。')
+    end
+  end
+
+  def destroy
+    if @price_table.destroy
+      flash[:success] = '削除しました。'
+      redirect_to index
+    else
+      render_index('削除に失敗しました。')
     end
   end
 
   private
 
+    def set_price_table
+      @price_table = PriceTable.find(params[:id])
+    end
+
     def price_table_params
       params
         .require(:price_table)
-        .permit(:category_id, :item_name, :specification, :unit, :unit_price, :remark)
+        .permit(:category_id, :price_table_id, :item_name, :specification, :unit, :unit_price, :remark)
         .merge(user_id: current_user.id)
+    end
+
+    def render_index(msg)
+      flash.now[:alert] = msg
+      @price_tables = current_user.price_tables
+      render 'index'
     end
 end
