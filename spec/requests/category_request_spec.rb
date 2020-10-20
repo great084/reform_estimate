cf20baa06c756290f043980e28e18c8a93925498
great084 <great084@gmail.com>
2020-10-20 20:37:56 +0900

    deleteアクションのテストを記載

--- spec/requests/category_request_spec.rb
+++ spec/requests/category_request_spec.rb
@@ -73,6 +73,37 @@ RSpec.describe "Categories", type: :request do
         expect(@category1.reload.category_name).to eq old_category_name
       end
     end
+
+    describe "#destroy" do
+      it "responds successfully with valid data" do
+        delete category_path(@category1)
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to categories_path
+      end
+
+      it "decrease 1 data when valid data" do
+        expect{
+          delete category_path(@category1)
+        }.to change(@user.categories, :count).by(-1)  
+      end
+
+      # 対象のレコードが既に存在しない場合のエラー処理がないので書くべき
+      xit "responds successfully with invalid data" do
+        @category1.destroy
+        delete category_path(@category1)
+        # binding.pry
+        expect(response).to be_successful
+        expect(response).to have_http_status "200"
+      end
+
+      it "not update a category values with invalid data" do
+        # binding.pry
+        old_category_name = @category1.category_name
+        put category_path(@category1), params: { category: {category_name: @category2.category_name } }
+        # expect{ @category1 }.not_to change{ @category1.reload }
+        expect(@category1.reload.category_name).to eq old_category_name
+      end
+    end
   end
 
   context "as no authenticated user" do
