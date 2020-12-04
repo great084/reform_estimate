cfdceb2ee7cb0b93da03718e111ae121385d0048
great084 <great084@gmail.com>
2020-12-04 19:20:33 +0900

    見積もり明細の更新と削除を実装

--- app/controllers/estimate_details_controller.rb
+++ app/controllers/estimate_details_controller.rb
@@ -2,10 +2,11 @@ class EstimateDetailsController < ApplicationController
   def create
     @estimate_detail = EstimateDetail.new(estimate_detail_params)
     estimate = @estimate_detail.estimate_category.estimate
+    estimate_category = @estimate_detail.estimate_category
     # binding.pry
     if @estimate_detail.save
       flash[:success] = '見積明細を登録しました'
-      redirect_to estimate_path(estimate)
+      redirect_to estimate_path(estimate, estimate_category_id: estimate_category)
     else
       # エラー処理は今後書く
       flash.now[:danger] = '見積明細の登録できませんでした'
@@ -13,9 +14,29 @@ class EstimateDetailsController < ApplicationController
   end
 
   def update
+    @estimate_detail = EstimateDetail.find(params[:id])
+    estimate = @estimate_detail.estimate_category.estimate
+    estimate_category = @estimate_detail.estimate_category
+    if @estimate_detail.update(estimate_detail_params)
+      flash[:success] = '見積明細を更新しました'
+      redirect_to estimate_path(estimate, estimate_category_id: estimate_category)
+    else
+      # エラー処理は今後書く
+      flash.now[:danger] = '見積明細の更新できませんでした'
+    end
   end
 
   def destroy
+    @estimate_detail = EstimateDetail.find(params[:id])
+    estimate = @estimate_detail.estimate_category.estimate
+    estimate_category = @estimate_detail.estimate_category
+    if @estimate_detail.destroy
+      flash[:success] = '見積明細を削除しました'
+      redirect_to estimate_path(estimate, estimate_category_id: estimate_category)
+    else
+      # エラー処理は今後書く
+      flash.now[:danger] = '見積明細の削除できませんでした'
+    end
   end
 
   def details
