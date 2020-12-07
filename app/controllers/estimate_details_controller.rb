529026e2b215c51fa888075e8df1db5740fd1c10
great084 <great084@gmail.com>
2020-12-08 00:41:31 +0900

    共通処理のメソッド化

--- app/controllers/estimate_details_controller.rb
+++ app/controllers/estimate_details_controller.rb
@@ -1,12 +1,12 @@
 class EstimateDetailsController < ApplicationController
+  before_action :estimate_detail, only: %i[create update destroy]
+  before_action :estimate_category, only: %i[create update destroy]
+  before_action :estimate, only: %i[create update destroy]
+
   def create
-    @estimate_detail = EstimateDetail.new(estimate_detail_params)
-    estimate = @estimate_detail.estimate_category.estimate
-    estimate_category = @estimate_detail.estimate_category
-    # binding.pry
     if @estimate_detail.save
       flash[:success] = '見積明細を登録しました'
-      redirect_to estimate_path(estimate, estimate_category_id: estimate_category)
+      redirect_to estimate_path(@estimate, estimate_category_id: @estimate_category)
     else
       # エラー処理は今後書く
       flash.now[:danger] = '見積明細の登録できませんでした'
@@ -14,12 +14,9 @@ class EstimateDetailsController < ApplicationController
   end
 
   def update
-    @estimate_detail = EstimateDetail.find(params[:id])
-    estimate = @estimate_detail.estimate_category.estimate
-    estimate_category = @estimate_detail.estimate_category
     if @estimate_detail.update(estimate_detail_params)
       flash[:success] = '見積明細を更新しました'
-      redirect_to estimate_path(estimate, estimate_category_id: estimate_category)
+      redirect_to estimate_path(@estimate, estimate_category_id: @estimate_category)
     else
       # エラー処理は今後書く
       flash.now[:danger] = '見積明細の更新できませんでした'
@@ -27,12 +24,9 @@ class EstimateDetailsController < ApplicationController
   end
 
   def destroy
-    @estimate_detail = EstimateDetail.find(params[:id])
-    estimate = @estimate_detail.estimate_category.estimate
-    estimate_category = @estimate_detail.estimate_category
     if @estimate_detail.destroy
       flash[:success] = '見積明細を削除しました'
-      redirect_to estimate_path(estimate, estimate_category_id: estimate_category)
+      redirect_to estimate_path(@estimate, estimate_category_id: @estimate_category)
     else
       # エラー処理は今後書く
       flash.now[:danger] = '見積明細の削除できませんでした'
@@ -49,5 +43,21 @@ class EstimateDetailsController < ApplicationController
       params
         .require(:estimate_detail)
         .permit(EstimateDetail::REGISTRABLE_ATTRIBUTES)
+    end
+
+    def estimate_detail
+      @estimate_detail = if params[:id]
+                           EstimateDetail.find(params[:id])
+                         else
+                           EstimateDetail.new(estimate_detail_params)
+                         end
+    end
+
+    def estimate_category
+      @estimate_category = estimate_detail.estimate_category
+    end
+
+    def estimate
+      @estimate = estimate_detail.estimate_category.estimate
     end
 end
